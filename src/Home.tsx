b4ef47c552219f5a65d61d236f532c813f6d30b2
Clément Sicard <c.clement.sicard@northvolt.com>
2023-08-13 21:42:03 +0200

    home: adds searching query logic

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,21 +1,34 @@
 import React, { useState } from 'react'
-import { Stack } from '@chakra-ui/react'
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Stack,
+  Text,
+  VStack
+} from '@chakra-ui/react'
 import { SearchBar } from './components/SearchBar'
+import ApiResponse from './types/ApiResponse'
 
 export const Home: React.FC = () => {
-  const [apiResult, setApiResult] = useState<string | null>(null)
+  const [data, setData] = useState<ApiResponse | null>(null)
   const [isSearching, setIsSearching] = useState(false)
+  const [isError, setIsError] = useState(false)
+  const [error, setError] = useState<any>(null)
 
   const handleSearch = async (query: string): Promise<void> => {
     console.log('Search query:', query)
 
     try {
       setIsSearching(true)
+      setIsError(false)
       const url = `http://localhost:80/search?q=${query}`
-      const response = await fetch(url)
-        .then(res => res.text())
-        .then(text => setApiResult(text))
+      const response = await fetch(url).then(response => response.json())
+      setData(response)
     } catch (error) {
+      setError(error)
+      setIsError(true)
       console.error('Error fetching data from API:', error)
     }
     setIsSearching(false)
@@ -23,11 +36,43 @@ export const Home: React.FC = () => {
 
   return (
     <>
-      <Stack spacing={4} direction='row' align='center' m='1rem'>
+      <Stack spacing={2} direction='column' align='center' m='1rem'>
         <SearchBar onSearch={handleSearch} isSearching={isSearching} />
       </Stack>
+      {isError && (
+        <div>
+          <VStack spacing={4} m='1rem'>
+            <Alert status='error' variant='left-accent'>
+              <AlertIcon />
+              <AlertTitle>Oops... </AlertTitle>
+              <AlertDescription>
+                <Text>
+                  There was an error fetching the API: {error.message}
+                </Text>
+              </AlertDescription>
+            </Alert>
+            <Text align='start' w='100'></Text>
+          </VStack>
+        </div>
+      )}
       {/* You can now use apiResult here in the Home component */}
-      {apiResult && <div>Received data: {apiResult}</div>}
+      {data && !isError && (
+        <div>
+          <VStack spacing={4} m='1rem'>
+            <Alert
+              status={data.total > 0 ? 'success' : 'warning'}
+              variant='left-accent'
+            >
+              <AlertIcon />
+              <Text>
+                Search returned <b> {data.total.toLocaleString(undefined)} </b>{' '}
+                results.
+              </Text>
+            </Alert>
+            <Text align='start' w='100'></Text>
+          </VStack>
+        </div>
+      )}
     </>
   )
 }
