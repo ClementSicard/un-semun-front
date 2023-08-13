b85c85322e78a0fa8ba08d869988e8380e9c833d
Clément Sicard <c.clement.sicard@northvolt.com>
2023-08-13 21:41:41 +0200

    types: ApiResponse type to unmarshall API request to

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,5 +1,41 @@
+interface RecordDownload {
+    العربية: string;
+    English: string;
+    Español: string;
+    Français: string;
+    Русский: string;
+    中文: string;
+}
+
+interface RecordSubject {
+    unbist: string[];
+    unbisn: string[];
+    misc: string[];
+}
+
+interface RecordCollection {
+    resource_type: string[];
+    un_bodies: string[];
+}
+
+interface ApiRecord {
+    id: string;
+    title: string;
+    location: string;
+    symbol: string;
+    publication_date: string;
+    authors: string[];
+    description: string;
+    downloads: RecordDownload;
+    subjects: RecordSubject;
+    agenda: string;
+    collections: RecordCollection;
+}
+
 interface ApiResponse {
-    data: string
+    total: number;
+    search_id: string;
+    records: ApiRecord[];
 }
 
 export default ApiResponse;
